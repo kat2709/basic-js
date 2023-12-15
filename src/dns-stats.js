ff3fb3c7794cc523f30bfafa4c4f237c208b5905
Katsiaryna Rubchanka <6923845@mail.ru>
2023-12-15 11:17:01 +0200

    feat: add solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    let arr = domains[i].split(".");
+    let arrReverse = arr.reverse();
+    let newArr = [];
+    for (let y = 0; y < arrReverse.length; y++) {
+      if (arrReverse[y - 1] !== undefined) {
+        newArr.push(`${newArr[newArr.length - 1]}.${arrReverse[y]}`);
+      } else {
+        newArr.push(`.${arrReverse[y]}`);
+      }
+    }
+    for (let x = 0; x < newArr.length; x++) {
+      if (obj[newArr[x]] !== undefined) {
+        obj[newArr[x]]++;
+      } else {
+        obj[newArr[x]] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = {
